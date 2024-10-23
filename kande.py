4d97f422e1ed557885e11d5885a6effb902f7522
MechaDragonX <theofficialrv@gmail.com>
2024-10-22 17:55:28 -0700

    Modify lists to be list of lists to improve codoe

--- kande.py
+++ kande.py
@@ -1,22 +1,17 @@
 # Data files and below variables can be moodified to met invidual
 
 # Lists representing Kanken levels 7, 6, 5, 4, 3, pre 2, and 2
+# A list of lists is used where x represents a given level
+# and y repsents the value 
 # 10-6 aren't included since it'd be useless data for me based on pretests
 # Pre-1 and 1 aren't common taken unless you care about obscure kanji
-kanken7 = []
-kanken6 = []
-kanken5 = []
-kanken4 = []
-kanken3 = []
-kankenp2 = []
-kanken2 = []
+kanken_lists = [ [], [], [], [], [], [], [] ]
+
 
 # Lists representing JLPT N5-2
 # I'm not taking N1 yet, so I haven't included it in the data yet
-n5 = []
-n4 = []
-n3 = []
-n2 = []
+# As with the Kanken ones, a list of lists is used
+jlpt_lists = [ [], [], [], [] ]
 
 
 # path = input file path
@@ -36,39 +31,26 @@ def gen_kanji_list(line: str) -> list:
 
 # path = input file path
 def gen_kanken_lists(path: str):
-    global kanken7
-    global kanken6
-    global kanken5
-    global kanken4
-    global kanken3
-    global kankenp2
-    global kanken2
+    global kanken_lists
 
-    kanken = read_file(path)
-    # The following lines expect a specific number of lines in the same position as the var list
-    kanken7 = gen_kanji_list(kanken[0])
-    kanken6 = gen_kanji_list(kanken[1])
-    kanken5 = gen_kanji_list(kanken[2])
-    kanken4 = gen_kanji_list(kanken[3])
-    kanken3 = gen_kanji_list(kanken[4])
-    kankenp2 = gen_kanji_list(kanken[5])
-    kanken2 = gen_kanji_list(kanken[6])
+    kanken_file = read_file(path)
+
+    for i in range(7):
+        kanken_lists[i] = gen_kanji_list(kanken_file[i])
 
 
 # path = input file path
 def gen_jlpt_lists(path: str):
-    global n5
-    global n4
-    global n3
-    global n2
+    global jlpt_lists
 
-    jlpt = read_file(path)
-    # The following lines expect a specific number of lines in the same position as the var list
-    n5 = gen_kanji_list(jlpt[0])
-    n4 = gen_kanji_list(jlpt[1])
-    n3 = gen_kanji_list(jlpt[2])
-    n2 = gen_kanji_list(jlpt[3])
+    jlpt_file = read_file(path)
+
+    for i in range(4):
+        jlpt_lists[i] = gen_kanji_list(jlpt_file[i])
 
 
 gen_kanken_lists('data/kanken.txt')
 gen_jlpt_lists('data/jlpt.txt')
+
+print(jlpt_lists[0])
+print(jlpt_lists[2])
